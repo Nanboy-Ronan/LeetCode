048ccaeec3b36745c92ef4ce1e778eeb17cf8c9a
Ruinan Jin <jinruinan@163.com>
2021-07-14 20:48:15 -0700

    Update BruteForce.py in according to Solution format.

--- LC53_MaximumSubarray/BruteForce/BruteForce.py
+++ LC53_MaximumSubarray/BruteForce/BruteForce.py
@@ -9,16 +9,16 @@
 
 from typing import List
 
-
-def maxSubArray(self, nums: List[int]) -> int:
-    # Brute Force
-    # Time Complexity: O(n^2)
-    # Space Complexity: O(1)
-    # This will exceed the time limit
-    result = float('-inf')
-    for i in range(len(nums)):
-        temp = 0
-        for j in range(i,len(nums)):
-            temp += nums[j]
-            result = max(temp,result)
-    return result
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # Brute Force
+        # Time Complexity: O(n^2)
+        # Space Complexity: O(1)
+        # This will exceed the time limit
+        result = float('-inf')
+        for i in range(len(nums)):
+            temp = 0
+            for j in range(i,len(nums)):
+                temp += nums[j]
+                result = max(temp,result)
+        return result
