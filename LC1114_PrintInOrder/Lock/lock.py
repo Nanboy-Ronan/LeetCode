a034422ca823d372c9d6f2b83b865cfabd2bad9b
Ruinan Jin <jinruinan@163.com>
2022-01-07 09:08:02 -0800

    Finish LC1114 with lock.py.

---
+++ LC1114_PrintInOrder/Lock/lock.py
@@ -0,0 +1,30 @@
+from threading import Lock
+
+class Foo:
+    def __init__(self):
+        self.l1 = Lock()
+        self.l2 = Lock()
+        self.l1.acquire()
+        self.l2.acquire()
+
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.l1.release()
+
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        
+        # printSecond() outputs "second". Do not change or remove this line.
+        self.l1.acquire()
+        printSecond()
+        self.l2.release()
+
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        
+        # printThird() outputs "third". Do not change or remove this line.
+        self.l2.acquire()
+        printThird()
