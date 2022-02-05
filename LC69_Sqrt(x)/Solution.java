ff06a14c290bfde488a684a790e5655fdc3e3e54
Ruinan Jin <jinruinan@163.com>
2022-02-04 23:20:49 -0800

    Finish LC69 with binary search in java.

---
+++ LC69_Sqrt(x)/Solution.java
@@ -0,0 +1,28 @@
+/*
+Given a non-negative integer x, compute and return the square root of x.
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+*/
+
+class Solution {
+    // Binary Search
+    // Time Complexity: O(logn)
+    // Space Complexity: O(1)
+    public int mySqrt(int x) {
+        if(x < 2){
+            return x;
+        }
+        int left = 1, right = x / 2;
+        while(left <= right){
+            int middle = left + (right - left) / 2;
+            if(middle*middle == x){
+                return (int)middle;
+            }else if(middle*middle > x){
+                right = (int)middle - 1;
+            }else{
+                left = (int)middle + 1;
+            }
+        }
+        return right;
+    }
+}
