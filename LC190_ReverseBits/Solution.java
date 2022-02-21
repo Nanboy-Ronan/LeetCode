e7482f8948ac8c7b9b89aab8edfc8615e25ace19
Ruinan Jin <jinruinan@163.com>
2022-02-20 17:37:44 -0800

    Finish LC190 with javascript. Update time complexity of java solution.

--- LC190_ReverseBits/Solution.java
+++ LC190_ReverseBits/Solution.java
@@ -5,7 +5,7 @@ Reverse bits of a given 32 bits unsigned integer.
 public class Solution {
     // you need treat n as an unsigned value
     // Bit wise Operation
-    // Time complexity: O(logn)
+    // Time complexity: O(1)
     // Space complexity: O(1)
     public int reverseBits(int n) {
         int result = 0;
---
+++ LC190_ReverseBits/bitWise.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+
+ var reverseBits = function(n) {
+    // Bit wise Operation
+    // Time complexity: O(1)
+    // Space complexity: O(1)
+    let result = 0
+    for(let i = 0; i < 32; i++){
+       result <<= 1
+       result += n & 1
+       n >>= 1
+   }
+   return result >>> 0 // keep result to be positive
+};
