d0091723ec681b215a5ee07ed7c9c29c174139f1
Ruinan Jin <jinruinan@163.com>
2022-02-24 22:29:33 -0800

    Finish LC215 with java.

---
+++ LC215_KthLargestElementInArray/QuickSelect/Solution.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    // Quick Select
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+    public int findKthLargest(int[] nums, int k) {
+        int left = 0;
+        int right = nums.length - 1;
+        List<Integer> num = new ArrayList<>();
+        while(true){
+            int pivot = partition(nums, left, right);
+            if(pivot == k - 1) return nums[pivot];
+            if(pivot > k - 1) right = pivot - 1;
+            else left = pivot + 1;
+            
+        }
+    }
+
+
+    void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    int partition(int[] nums, int left, int right){
+        int pivot = left;
+        for(int i = left; i < right; i++){
+            if(nums[i] > nums[right]){
+                swap(nums, i, pivot);
+                pivot++;
+            }
+        }
+        swap(nums, pivot, right);
+        return pivot;
+    }
+
+}
