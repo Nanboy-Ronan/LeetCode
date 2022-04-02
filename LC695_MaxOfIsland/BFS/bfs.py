05f59af4b3507ba5d36b1dd6bb00c9e4a4b59544
Ruinan Jin <jinruinan@163.com>
2022-04-01 23:51:38 -0700

    Update bfs.py.

--- LC695_MaxOfIsland/BFS/bfs.py
+++ LC695_MaxOfIsland/BFS/bfs.py
@@ -17,14 +17,20 @@ class Solution:
                     counter = 0
                     queue = deque()
                     queue.append([i,j])
+                    grid[i][j] = 0
                     while len(queue) > 0:
                         m, n = queue.popleft()
-                        grid[m][n] = 0
                         counter += 1
                         for dir in dirs:
                             x = m + dir[0]
                             y = n + dir[1]
                             if x >= 0 and x < numRow and y >= 0 and y < numCol and grid[x][y]:
+                                grid[x][y] = 0
                                 queue.append([x,y])
                     result = result if result >= counter else counter
         return result
+
+##################################### Test ##########################################
+test = Solution()
+result = test.maxAreaOfIsland([[1,1,0,0,0],[1,1,0,0,0],[0,0,0,1,1],[0,0,0,1,1]])
+print(result)
